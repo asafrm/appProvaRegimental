09ff6aae08ec092392d90e9b52433e5ffdd32bb0
Asafe <asafe.rodrigues@cs.udf.edu.br>
2026-06-07 19:39:51 -0300

    Alteração tela alterar carro xaml.cs

--- Views/TelaAlterarCarro.xaml.cs
+++ Views/TelaAlterarCarro.xaml.cs
@@ -1,13 +1,43 @@
-private async void OnSalvarClicked(object sender, EventArgs e)
-{
-    string modelo = txtModelo.Text;
-    string marca = txtMarca.Text;
-    string ano = txtAno.Text;
-    string placa = txtPlaca.Text;
+using appProvaRegimental.Model;
+namespace appProvaRegimental.Views;
 
-    await DisplayAlert(
-        "Sucesso",
-        "Dados alterados com sucesso!",
-        "OK"
-    );
+public partial class TelaAlterarCarro : ContentPage
+{
+    public TelaAlterarCarro()
+    {
+        InitializeComponent();
+    }
+    private async void ToolbarItemClickedSalvar(object sender, EventArgs e)
+    {
+        try
+        {
+            Carro CarroAnexado = BindingContext as Carro;
+            if (string.IsNullOrWhiteSpace(txtNomeCarro.Text))
+            {
+                await DisplayAlert("Erro", "Por favor, preencha o nome do carro!!", "OK");
+                txtNomeCarro.Focus();
+            }
+            else if (string.IsNullOrWhiteSpace(txtPlacaCarro.Text))
+            {
+                await DisplayAlert("Erro", "Por favor, preencha a placa do carro!!", "OK");
+                txtPlacaCarro.Focus();
+            }
+            else
+            {
+                Carro carro1 = new Carro()
+                {
+                    carID = CarroAnexado.carID,
+                    carNome = txtNomeCarro.Text,
+                    carPlaca = txtPlacaCarro.Text,
+                };
+                await App.Database.Update(carro1);
+                await DisplayAlert("Sucesso", "Carro atualizado com sucesso!!", "OK");
+                await Navigation.PushAsync(new TelaListaCarro());
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Ocorreu um erro ao atualizar o carro: {ex.Message}", "OK");
+        }
+    }
 }
