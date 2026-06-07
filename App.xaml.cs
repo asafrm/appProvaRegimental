4ade340c1b9c242872c14de10d05a23eb7d9376c
Asafe <asafe.rodrigues@cs.udf.edu.br>
2026-06-07 19:36:07 -0300

    Alteração no app.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
+﻿using appProvaRegimental.DAl;
+using appProvaRegimental.Views;
 namespace appProvaRegimental
 {
     public partial class App : Application
     {
+        static crudSQLite? database;
+
+        public static crudSQLite Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "carros.db3");
+                    database = new crudSQLite(path);
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-        }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage = new NavigationPage(new TelaListaCarro());
         }
     }
 }
